d15a2c221539949c75cd0669a5cd636c70dbcc3a
u01ai11 <a.irvine.10@aberdeen.ac.uk>
2017-08-09 16:29:44 +0100

    Altered recorded coords and updated errors
    - now we expose X,Y,Z of camera in three.js scene instead of Long/Lat
    - updated stylesheet location error
    - moved function for rendering chart

--- src/jquery.valient360.js
+++ src/jquery.valient360.js
@@ -712,8 +712,9 @@ three.js r65 or higher
             this._renderer.clear();
             this._renderer.render( this._scene, this._camera );
                     //add the coordinates to player data variable
-            playerData.lon = this._lon
-            playerData.lat = this._lat
+            playerData.x = this.camera.position.x
+            playerData.y = this.camera.position.y
+            playerData.z = this.camera.position.z
 
         },
 
